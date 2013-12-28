e3101d8851d6e5aafa27e91ab4ea74f751bd1243
Alex Clark <aclark@aclark.net>
2013-12-28 10:16:15 -0500

    Add meta

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
-    name='hmc_theme'   
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    }
+    name='hmc_theme',
+    packages=find_packages(),
 )
